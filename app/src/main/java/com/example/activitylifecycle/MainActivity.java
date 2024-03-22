7e6326b10098e9d39ae6750600847d496238470c
Adity <2123060@leedstrinity.ac.uk>
2024-03-22 04:27:56 +0000

    gwgwg

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
